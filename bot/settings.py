2a2eab9b1809f59b67b46464a0ad5d18020a1f78
Vadim Ryazanov <vadimvoronezh@yandex.ru>
2021-05-10 18:14:58 +0300

    use pvc to keep files

--- bot/settings.py
+++ bot/settings.py
@@ -7,5 +7,3 @@ BOT_TOKEN = os.environ['BOT_TOKEN']
 BOT_ADMIN_GROUP = int(os.environ['BOT_ADMIN_GROUP'])
 
 STORAGE_PATH = pathlib.Path(os.environ['STORAGE_PATH'])
-
-STOCKS_BASE_URL = os.environ['STOCKS_BASE_URL']
